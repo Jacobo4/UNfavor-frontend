8c880eee391c87601585b7265464ec4e6f47d826
nquirogac <nquirogac@unal.edu.co>
2023-03-26 23:31:28 -0500

    css profile

--- src/pages/Profile/Raiting.tsx
+++ src/pages/Profile/Raiting.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import styles from './Raiting.module.css'
+import { BsStarFill,BsStarHalf } from 'react-icons/bs';
+
 
 export default function Raiting() {
   return (
-    <div>Raiting</div>
+    <div className={styles['stars']}>
+        <BsStarFill/>
+        <BsStarFill/>
+        <BsStarFill/>
+        <BsStarFill/>
+        <BsStarFill/>
+    </div>
   )
 }
