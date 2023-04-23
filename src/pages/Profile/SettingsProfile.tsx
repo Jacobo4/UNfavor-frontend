35bc94e080becda0e27135294ce3a58c53478d2e
nquirogac <nquirogac@unal.edu.co>
2023-04-23 16:59:31 -0500

    update form

--- src/pages/Profile/SettingsProfile.tsx
+++ src/pages/Profile/SettingsProfile.tsx
@@ -4,8 +4,7 @@ import React from "react";
 import avatar from "@assets/images/avatar.png";
 import { MdSettings } from "react-icons/md";
 import SettingForm from "./Forms/SettingForm";
-import FavorForm from "./Forms/FavorForm";
-import FilterForm from "./Forms/FilterForm";
+
 
 const SettingsProfile: React.FC = () => {
   return (
@@ -26,14 +25,7 @@ const SettingsProfile: React.FC = () => {
           <SettingForm/>
         </div>
       </div>
-      <div className={styles.container}>
-        <h2>Editar favor</h2>
-        <FavorForm />
-      </div>
-      <div className={styles.container}>
-        <h2>Editar filtros</h2>
-        <FilterForm />
-      </div>
+      
     </div>
   );
 };
