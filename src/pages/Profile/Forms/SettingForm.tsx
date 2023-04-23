d8a287541cfb80375afb79dd289ba4e969aa8857
nquirogac <nquirogac@unal.edu.co>
2023-04-23 17:34:47 -0500

    fix select

--- src/pages/Profile/Forms/SettingForm.tsx
+++ src/pages/Profile/Forms/SettingForm.tsx
@@ -39,101 +39,121 @@ export default function Form() {
     <div>
       <form onSubmit={onSubmit}>
         <div>
-          <h3>Nombre completo: </h3>
-          <input
-            type="text"
-            {...register("fullName", {
-              required: true,
-            })}
-            placeholder="Nombre completo"
-            
-          />
+          <label>Nombre completo: </label>
+          <div>
+            <input
+              type="text"
+              {...register("fullName", {
+                required: true,
+              })}
+              placeholder="Nombre completo"
+            />
 
-          {errors.fullName?.type === "required" && (
-            <p>El campo Nombre es obigatorio</p>
-          )}
-          <h3>Contraseña: </h3>
-          <input
-            type="password"
-            {...register("password", {
-              required: true,
-            })}
-            placeholder="Contraseña"
-            
-          />
+            {errors.fullName?.type === "required" && (
+              <p>El campo Nombre es obigatorio</p>
+            )}
+          </div>
+          <label>Contraseña: </label>
+          <div>
+            <input
+              type="password"
+              {...register("password", {
+                required: true,
+              })}
+              placeholder="Contraseña"
+            />
 
-          {errors.password?.type === "required" && (
-            <p>El campo Contraseña es obigatorio</p>
-          )}
-          <h3>Edad: </h3>
-          <input
-            type="text"
-            {...register("age", {
-              required: true,
-            })}
-            placeholder="Edad"
-            
-          />
+            {errors.password?.type === "required" && (
+              <p>El campo Contraseña es obigatorio</p>
+            )}
+          </div>
+          <label>Edad: </label>
+          <div>
+            <input
+              type="text"
+              {...register("age", {
+                required: true,
+              })}
+              placeholder="Edad"
+            />
 
-          {errors.age?.type === "required" && (
-            <p>El campo Edad es obigatorio</p>
-          )}
-          <h3>Teléfono: </h3>
-          <input
-            placeholder="Telefono"
-            type="text"
-            {...register("phone", {
-              required: true,
-            })}
-          />
-          {errors.phone?.type === "required" && (
-            <p>El campo Telefono es obigatorio</p>
-          )}
-<h2>Editar favor</h2>
-<h3>Titulo del favor: </h3>
-          <input
-            type="text"
-            {...register("tittle", {
-              required: true,
-            })}
-            placeholder="Titulo del favor"
-            
-          />
+            {errors.age?.type === "required" && (
+              <p>El campo Edad es obigatorio</p>
+            )}
+          </div>
+          <label>Teléfono: </label>
+          <div>
+            <input
+              placeholder="Telefono"
+              type="text"
+              {...register("phone", {
+                required: true,
+              })}
+            />
+            {errors.phone?.type === "required" && (
+              <p>El campo Telefono es obigatorio</p>
+            )}
+          </div>
+          <h2>Editar favor</h2>
+          <label>Titulo del favor: </label>
+          <div>
+            <input
+              type="text"
+              {...register("tittle", {
+                required: true,
+              })}
+              placeholder="Titulo del favor"
+            />
 
-          {errors.tittle?.type === "required" && (
-            <p>El campo Titulo es obigatorio</p>
-          )}
-          <h3>Descripción: </h3>
-          <textarea
-            {...register("description", {
-              required: true,
-            })}
-            placeholder="Descripción"
-            
-          />
+            {errors.tittle?.type === "required" && (
+              <p>El campo Titulo es obigatorio</p>
+            )}
+          </div>
+          <label>Descripción: </label>
+          <div>
+            <textarea
+              {...register("description", {
+                required: true,
+              })}
+              placeholder="Descripción"
+            />
 
-          {errors.description?.type === "required" && (
-            <p>El campo Descripción es obigatorio</p>
-          )}
-          <h3>Categoria: </h3>
+            {errors.description?.type === "required" && (
+              <p>El campo Descripción es obigatorio</p>
+            )}
+          </div>
+          <label>Categoria: </label>
           <div className={styles.select}>
-            <SelectCategory />
+            <select {...register("category", { required: true })}>
+              <option value="">Select...</option>
+              <option value="Limpieza">Limpieza</option>
+              <option value="Académico">Académico</option>
+            </select>
+
+            {errors.category?.type === "required" && (
+              <p>El campo Categoria es obigatorio</p>
+            )}
           </div>
           <h2>Editar filtro</h2>
-          <h3>Categoria: </h3>
+          <label>Categoria: </label>
           <div className={styles.select}>
-            <SelectCategory />
+            <select {...register("tipe", { required: true })}>
+              <option value="">Select...</option>
+              <option value="Limpieza">Limpieza</option>
+              <option value="Académico">Académico</option>
+            </select>
+
+            {errors.tipe?.type === "required" && (
+              <p>El campo Categoria es obigatorio</p>
+            )}
           </div>
-          {errors.tipe?.type === "required" && (
-            <p>El campo Categoria es obigatorio</p>
-          )}
-          <h3>Distancia: </h3>
+          <label>Distancia: </label>
           <div className={styles.slider}>
-            <DistanceSlider/>
+            <DistanceSlider />
           </div>
-          <button type="submit">
-            Guardar cambios
-          </button>
+          <div>
+            <button type="submit">Guardar cambios</button>
+          </div>
         </div>
       </form>
     </div>
