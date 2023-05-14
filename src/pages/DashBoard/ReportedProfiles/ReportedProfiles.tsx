f51a50732e6a5ff98e9e5643517bbeb4d44beda8
Juan Camilo <juzambranol@unal.edu.co>
2023-05-13 20:30:16 -0500

    ReportProfiles layout

--- src/pages/DashBoard/ReportedProfiles/ReportedProfiles.tsx
+++ src/pages/DashBoard/ReportedProfiles/ReportedProfiles.tsx
@@ -1,9 +1,21 @@
+import ReportCard from "./ReportCard";
 import styles from "./ReportedProfiles.module.css";
 import React from "react";
 const ReportedProfiles: React.FC = () => {
+    const dataDummie=[
+        {
+            reportedProfile: "Juan123",
+            reportedPhotoProfile: "https://www.kindpng.com/picc/m/429-4296037_empty-profile-picture-jpg-hd-png-download.png",
+            reporterProfile: "Jacobo123",
+            reporterPhotoProfile: "https://www.kindpng.com/picc/m/429-4296037_empty-profile-picture-jpg-hd-png-download.png",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non tristique velit, non aliquam sem. In hac habitasse platea dictumst. Morbi sed lobortis nibh. In efficitur dolor vel fermentum sagittis. Mauris et ex ut orci ultricies posuere. Nulla dapibus eget lacus venenatis bibendum. Nunc maximus, purus dapibus ornare blandit, quam felis tincidunt turpis, eget congue lectus massa sed risus. Etiam consectetur purus id leo vestibulum, non dignissim ex lobortis. Sed eget tortor hendrerit, convallis felis sollicitudin, placerat nibh. Suspendisse et erat arcu.",
+        },]
   return (
     <div className={styles.ReportedProfiles}>
-        Perfiles Reportados
+        <div className={styles.ReportCardsContainer}>
+            
+          {dataDummie.map((info, index) => <ReportCard{...info}/> )}
+        </div>
     </div>
   );
 };
