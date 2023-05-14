5bd8431826de9dd25e4c92cf196ee5c778327a6a
Juan Camilo <juzambranol@unal.edu.co>
2023-05-14 00:19:24 -0500

    ReportedProfiles Layout fixes

--- src/pages/DashBoard/ReportedProfiles/ReportedProfiles.tsx
+++ src/pages/DashBoard/ReportedProfiles/ReportedProfiles.tsx
@@ -9,11 +9,38 @@ const ReportedProfiles: React.FC = () => {
             reporterProfile: "Jacobo123",
             reporterPhotoProfile: "https://www.kindpng.com/picc/m/429-4296037_empty-profile-picture-jpg-hd-png-download.png",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non tristique velit, non aliquam sem. In hac habitasse platea dictumst. Morbi sed lobortis nibh. In efficitur dolor vel fermentum sagittis. Mauris et ex ut orci ultricies posuere. Nulla dapibus eget lacus venenatis bibendum. Nunc maximus, purus dapibus ornare blandit, quam felis tincidunt turpis, eget congue lectus massa sed risus. Etiam consectetur purus id leo vestibulum, non dignissim ex lobortis. Sed eget tortor hendrerit, convallis felis sollicitudin, placerat nibh. Suspendisse et erat arcu.",
-        },]
+        },
+        {
+          reportedProfile: "Juan123",
+          reportedPhotoProfile: "https://www.kindpng.com/picc/m/429-4296037_empty-profile-picture-jpg-hd-png-download.png",
+          reporterProfile: "Jacobo123",
+          reporterPhotoProfile: "https://www.kindpng.com/picc/m/429-4296037_empty-profile-picture-jpg-hd-png-download.png",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non tristique velit, non aliquam sem. In hac habitasse platea dictumst. Morbi sed lobortis nibh. In efficitur dolor vel fermentum sagittis. Mauris et ex ut orci ultricies posuere. Nulla dapibus eget lacus venenatis bibendum. Nunc maximus, purus dapibus ornare blandit, quam felis tincidunt turpis, eget congue lectus massa sed risus. Etiam consectetur purus id leo vestibulum, non dignissim ex lobortis. Sed eget tortor hendrerit, convallis felis sollicitudin, placerat nibh. Suspendisse et erat arcu.",
+      },{
+        reportedProfile: "Juan123",
+        reportedPhotoProfile: "https://www.kindpng.com/picc/m/429-4296037_empty-profile-picture-jpg-hd-png-download.png",
+        reporterProfile: "Jacobo123",
+        reporterPhotoProfile: "https://www.kindpng.com/picc/m/429-4296037_empty-profile-picture-jpg-hd-png-download.png",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non tristique velit, non aliquam sem. In hac habitasse platea dictumst. Morbi sed lobortis nibh. In efficitur dolor vel fermentum sagittis. Mauris et ex ut orci ultricies posuere. Nulla dapibus eget lacus venenatis bibendum. Nunc maximus, purus dapibus ornare blandit, quam felis tincidunt turpis, eget congue lectus massa sed risus. Etiam consectetur purus id leo vestibulum, non dignissim ex lobortis. Sed eget tortor hendrerit, convallis felis sollicitudin, placerat nibh. Suspendisse et erat arcu.",
+    },
+    {
+      reportedProfile: "Juan123",
+      reportedPhotoProfile: "https://www.kindpng.com/picc/m/429-4296037_empty-profile-picture-jpg-hd-png-download.png",
+      reporterProfile: "Jacobo123",
+      reporterPhotoProfile: "https://www.kindpng.com/picc/m/429-4296037_empty-profile-picture-jpg-hd-png-download.png",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non tristique velit, non aliquam sem. In hac habitasse platea dictumst. Morbi sed lobortis nibh. In efficitur dolor vel fermentum sagittis. Mauris et ex ut orci ultricies posuere. Nulla dapibus eget lacus venenatis bibendum. Nunc maximus, purus dapibus ornare blandit, quam felis tincidunt turpis, eget congue lectus massa sed risus. Etiam consectetur purus id leo vestibulum, non dignissim ex lobortis. Sed eget tortor hendrerit, convallis felis sollicitudin, placerat nibh. Suspendisse et erat arcu.",
+  },]
   return (
     <div className={styles.ReportedProfiles}>
+      <div className={styles.headInfo}>
+        <h1>
+              Control de Reportes
+          </h1>
+          <h2>
+            Aprueba o Rechaza Reportes por parte de usuarios.
+          </h2>
+      </div>
         <div className={styles.ReportCardsContainer}>
-            
           {dataDummie.map((info, index) => <ReportCard{...info}/> )}
         </div>
     </div>
