8e90a5e916fe5443ac3749a0f4223d42574bd71d
nquirogac <nquirogac@unal.edu.co>
2023-05-16 10:53:23 -0500

    fix statistics

--- src/pages/DashBoard/Dashboard/Dashboard.tsx
+++ src/pages/DashBoard/Dashboard/Dashboard.tsx
@@ -12,71 +12,70 @@ const Dashboard: React.FC = () => {
   const dispatch = useAppDispatch();
   const { adminInfo } = useAppSelector((state) => state.admin);
   console.log(adminInfo);
-  useEffect(() => {    
+  useEffect(() => {
     if (!adminInfo) {
-    
       dispatch(getAdminInfo());
-      
     }
   }, []);
   return (
     <div className={styles.Dashboard}>
-      
-      <div className={styles.firstContent}>
-        <div className={styles.singleDashboardCard}>
-          <div className={styles.cardContent}>
-            <h2>Total Favores Publicados</h2>
-            <span>{adminInfo.totalFavors}</span>
+      {adminInfo && (
+        <>
+          <div className={styles.firstContent}>
+            <div className={styles.singleDashboardCard}>
+              <div className={styles.cardContent}>
+                <h2>Total Favores Publicados</h2>
+                <span>{adminInfo.totalFavors}</span>
+              </div>
+              <span className={styles.cardIcon}>
+                <BsFilePost />
+              </span>
+            </div>
+            <div className={styles.singleDashboardCard}>
+              <div className={styles.cardContent}>
+                <h2>Total de usuarios</h2>
+                <span>{adminInfo.totalUsers}</span>
+              </div>
+              <span className={styles.cardIcon}>
+                <FaUsers />
+              </span>
+            </div>
           </div>
-          <span className={styles.cardIcon}>
-            <BsFilePost />
-          </span>
-        </div>
-        <div className={styles.singleDashboardCard}>
-          <div className={styles.cardContent}>
-            <h2>Total de usuarios</h2>
-            <span>{adminInfo.totalUsers}</span>
+          <div className={styles.secondContent}>
+            <div className={styles.singleDashboardCard}>
+              <div className={styles.cardContent}>
+                <h2>Favores denegados</h2>
+                <span>{adminInfo.totalDeniedFavors}</span>
+              </div>
+              <span className={styles.cardIcon}>
+                <FcCancel />
+              </span>
+            </div>
+            <div className={styles.singleDashboardCard}>
+              <div className={styles.cardContent}>
+                <h2>Favores en revisión</h2>
+                <span>{adminInfo.totalReviewingFavors}</span>
+              </div>
+              <span className={styles.cardIcon}>
+                <BsSearch />
+              </span>
+            </div>
+            <div className={styles.singleDashboardCard}>
+              <div className={styles.cardContent}>
+                <h2>Favores publicados</h2>
+                <span>{adminInfo.totalPublishedFavors}</span>
+              </div>
+              <span className={styles.cardIcon}>
+                <AiFillCheckCircle />
+              </span>
+            </div>
           </div>
-          <span className={styles.cardIcon}>
-            <FaUsers />
-          </span>
-        </div>
-      </div>
-      <div className={styles.secondContent}>
-        <div className={styles.singleDashboardCard}>
-          <div className={styles.cardContent}>
-            <h2>Favores denegados</h2>
-            <span>{adminInfo.totalDeniedFavors}</span>
+          <div className={styles.charts}>
+            <PieChart arr={adminInfo.userScore} />
+            <LineChart arr={adminInfo.favorsPerMonth} />
           </div>
-          <span className={styles.cardIcon}>
-            <FcCancel />
-          </span>
-        </div>
-        <div className={styles.singleDashboardCard}>
-          <div className={styles.cardContent}>
-            <h2>Favores en revisión</h2>
-            <span>{adminInfo.totalReviewingFavors}</span>
-          </div>
-          <span className={styles.cardIcon}>
-            <BsSearch />
-          </span>
-        </div>
-        <div className={styles.singleDashboardCard}>
-          <div className={styles.cardContent}>
-            <h2>Favores publicados</h2>
-            <span>{adminInfo.totalPublishedFavors}</span>
-          </div>
-          <span className={styles.cardIcon}>
-            <AiFillCheckCircle />
-          </span>
-        </div>
-      </div>
-
-      <div className={styles.charts}>
-        <PieChart arr={adminInfo.userScore} />
-        <LineChart arr={adminInfo.favorsPerMonth
-} />
-      </div>
+        </>
+      )}
     </div>
   );
 };
