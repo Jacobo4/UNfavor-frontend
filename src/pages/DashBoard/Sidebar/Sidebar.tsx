6782e11ffe6fc87a1a0caa6a9ddb27f91301069c
Juan Camilo <juzambranol@unal.edu.co>
2023-05-17 13:50:54 -0500

    Logout Admin

--- src/pages/DashBoard/Sidebar/Sidebar.tsx
+++ src/pages/DashBoard/Sidebar/Sidebar.tsx
@@ -4,11 +4,16 @@ import { NavLink, useNavigate } from "react-router-dom";
 import logo from "@assets/images/logo.png";
 import { AiOutlineLogout } from "react-icons/ai";
 import navLinks from '@assets/dummy-data/navLinksSideBar.jsx'
+import { logout } from "@store/auth/authSlice";
+import { useAppDispatch } from "@store/hooks";
 const Sidebar: React.FC = () => {
   
-
+  const dispatch= useAppDispatch();
   const navigate = useNavigate();
-  const handleLogout = () => {};
+  const handleLogout = () => {
+      dispatch(logout());
+      navigate("/");
+  };
   return (
       <div className={styles.Sidebar}>
         <div className={styles.SidebarTop}>
@@ -38,14 +43,12 @@ const Sidebar: React.FC = () => {
             </ul>
           </div>
           <div className={styles.SidebarBottom}>
-            <NavLink
-              to="/"
-              className={styles.SidebarBottomButton}
-              onClick={() => handleLogout()}
+            <a              className={styles.SidebarBottomButton}
+              onClick={handleLogout}
             >
               <AiOutlineLogout />
               <span>Cerrar Sesión</span>
-            </NavLink>
+            </a>
           </div>
         </div>
       </div>
