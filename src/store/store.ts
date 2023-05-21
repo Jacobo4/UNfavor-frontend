f93fb2479fb696a9b745b4fa9031a7c792578142
jacobo <juanjacoboib@gmail.com>
2023-05-21 15:53:59 -0500

    Resetting store on logout

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,25 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import {configureStore, ThunkAction, Action, combineReducers} from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 // Reducers
 import authReducer from './auth/authSlice';
 import userReducer from './user/userSlice';
 import adminReducer from './Admin/adminSlice';
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
+
+const combinedReducer = combineReducers({
+   auth: authReducer,
     user: userReducer,
     admin: adminReducer,
-  },
+});
+
+const rootReducer = (state, action) => {
+  if (action.type === 'user/logout') {
+    state = undefined;
+  }
+  return combinedReducer(state, action);
+};
+
+export const store = configureStore({
+  reducer: rootReducer,
   // Just for debbuging purposes
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
